8d1764311dd1ff7b8fc1bfd2e3524971d731c496
DRen <enriquecortez.111@gmail.com>
2022-12-08 12:10:51 -0600

    Nuevo gafrico agregado al archivo

--- pie.py
+++ pie.py
@@ -7,3 +7,19 @@ myexplode = [0.2, 0, 0, 0]
 
 plt.pie(y, labels = mylabels, explode = myexplode)
 plt.show() 
+
+#plot 1:
+x = np.array([0, 1, 2, 3])
+y = np.array([3, 8, 1, 10])
+
+plt.subplot(1, 2, 1)
+plt.plot(x,y)
+
+#plot 2:
+x = np.array([0, 1, 2, 3])
+y = np.array([10, 20, 30, 40])
+
+plt.subplot(1, 2, 2)
+plt.plot(x,y)
+
+plt.show()
